ef565b4d1fb4554c1ba4b5934ec6610c538cf70c
Christian Guegi <christian.guegi@gmail.com>
2019-12-19 14:33:52 +0100

    Update adb_pipeline.py

--- adb_pipeline.py
+++ adb_pipeline.py
@@ -9,7 +9,7 @@ args = {
     'email': ['<your-email-address>'],
     'email_on_failure' : True,
     'depends_on_past': False,
-	'databricks_conn_id': 'adb_workspace'
+    'databricks_conn_id': 'adb_workspace'
 }
 
 # DAG with Context Manager
